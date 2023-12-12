3d45b2653fcbe5051e322e47e42d339379a7ef89
amineguezmir <guezmiramine1@gmail.com>
2023-12-12 16:20:53 +0100

    register and login done

--- team-5-pinterest-clone/backend/database/config.js
+++ team-5-pinterest-clone/backend/database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: "localhost",
   user: "root",
-  password: "",
+  password: "amineGZ99",
   database: "pinterestclone",
 };
--- team-5-pinterest-clone/client/src/App.js
+++ team-5-pinterest-clone/client/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "../src/register/Register.jsx";
+import Login from "./login/Login.jsx";
+
 function App() {
-  return <div className="App"> test </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
