88d11a2305f4a8e521b9b4b6834c44e5370547e0
salimhanchaoui <s.hanchaoui@yahoo.fr>
2023-12-12 22:23:10 +0100

    loading

---
+++ team-5-pinterest-clone/client/src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+    const navigate = useNavigate();
+    const handleHomeBTN=()=>{
+        navigate('/all-posts')
+    }
+  return (
+    <div className='homebg'><h2 className='hometitle'>Welcome to Name– Where Every Image Tells a Story!</h2>
+    <p className='hometxt'>Unlock the beauty of visual storytelling with Name, your go-to destination for a captivating collection of images. Immerse yourself in a world where creativity knows no bounds, and each photograph is a window into the extraordinary.</p>
+    <button className='homebtn' onClick={handleHomeBTN}>See Posts</button></div>
+  )
+}
+
+export default Home
---
+++ team-5-pinterest-clone/client/src/Components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Nav() {
+  return (
+    <div className='nav'><div><img src="" alt="" />
+        <Link to="/" className="nav_home">Home</Link></div>
+        <div className='inptandbtn'><div style={{ position: 'relative', display: 'inline-block' }}>
+    <input type="text" placeholder='search' className='searchinpt' />
+    <button className='srchbtn'><i className="fa-solid fa-magnifying-glass"></i></button>
+  </div></div>
+        <div className='alltext'><Link to="/all-posts" className="nav_text">Posts</Link>
+        <Link to="/create" className="nav_text">Create</Link>
+        <Link to="/profile" className="nav_text"><img src="https://th.bing.com/th/id/OIP.BVbNgsb0pic_Ju-OKXrU3QAAAA?w=270&h=270&rs=1&pid=ImgDetMain" alt="" className='myimg'/></Link>
+        </div>
+        </div>
+  )
+}
+
+export default Nav
