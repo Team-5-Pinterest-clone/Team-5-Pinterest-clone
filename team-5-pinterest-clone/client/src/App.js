6e2c2e00ccd60c29a182c082f7e9ae72ccf1cb59
amineguezmir <guezmiramine1@gmail.com>
2023-12-13 10:22:06 +0100

    one post starter

--- team-5-pinterest-clone/client/src/App.js
+++ team-5-pinterest-clone/client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "../src/register/Register.jsx";
 import Login from "./login/Login.jsx";
+import OnePost from "../src/onePost/OnePost.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/onePost" element={<OnePost />} />
         </Routes>
       </BrowserRouter>
     </div>
