e3b003adf2280fbe1ce851ab5e65f6402e76029d
salmenkhelifi1 <salmen@base.tn>
2023-12-14 22:25:00 +0100

    nav onepost

--- team-5-pinterest-clone/client/src/Components/Nav.jsx
+++ team-5-pinterest-clone/client/src/Components/Nav.jsx
@@ -1,20 +1,246 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+// when user loged in
 
-function Nav() {
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import Avatar from "@mui/material/Avatar";
+import HomeIcon from "@mui/icons-material/Home";
+import CreateIcon from "@mui/icons-material/Create";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.black, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const StyledIconButton = styled(IconButton)(({ theme }) => ({
+  color: "#000",
+  "&:hover": {
+    backgroundColor: theme.palette.action.hover,
+    color: "#fff",
+  },
+}));
+
+const StyledMenuItem = styled(MenuItem)(({ theme }) => ({
+  color: "#000",
+  "&:hover": {
+    backgroundColor: theme.palette.action.hover,
+    color: "#000",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: theme.palette.common.black,
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: theme.palette.common.black,
+  "& input::placeholder": {
+    color: theme.palette.common.black,
+  },
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <StyledMenuItem onClick={handleMenuClose}>Profile</StyledMenuItem>
+      <StyledMenuItem onClick={handleMenuClose}>My account</StyledMenuItem>
+      <StyledMenuItem onClick={handleMenuClose}>log out</StyledMenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <StyledMenuItem>
+        <StyledIconButton
+          size="large"
+          aria-label="show all posts"
+          color="inherit"
+        >
+          <Badge color="error">
+            <HomeIcon />
+          </Badge>
+        </StyledIconButton>
+        <p>All Posts</p>
+      </StyledMenuItem>
+      <StyledMenuItem>
+        <StyledIconButton
+          size="large"
+          aria-label="Create post "
+          color="inherit"
+        >
+          <Badge color="error">
+            <CreateIcon />
+          </Badge>
+        </StyledIconButton>
+        <p>Create</p>
+      </StyledMenuItem>
+      <StyledMenuItem>
+        <Avatar
+          onClick={handleProfileMenuOpen}
+          alt="Remy Sharp"
+          href="https://pin.it/5LeSeoE"
+        />
+        <p>Profile</p>
+      </StyledMenuItem>
+    </Menu>
+  );
+
   return (
-    <div className='nav'><div><img src="" alt="" />
-        <Link to="/" className="nav_home">Home</Link></div>
-        <div className='inptandbtn'><div style={{ position: 'relative', display: 'inline-block' }}>
-    <input type="text" placeholder='search' className='searchinpt' />
-    <button className='srchbtn'><i className="fa-solid fa-magnifying-glass"></i></button>
-  </div></div>
-        <div className='alltext'><Link to="/all-posts" className="nav_text">Posts</Link>
-        <Link to="/createPost" className="nav_text">Create</Link>
-        <Link to="/profile" className="nav_text"><img src="https://th.bing.com/th/id/OIP.BVbNgsb0pic_Ju-OKXrU3QAAAA?w=270&h=270&rs=1&pid=ImgDetMain" alt="" className='myimg'/></Link>
-        </div>
-        </div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "rgba(251, 251, 251, 1)" }}
+      >
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: "none", sm: "block" }, color: "black" }}
+          >
+            PinTastic
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search user"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <StyledIconButton
+              size="large"
+              aria-label="show all posts"
+              color="inherit"
+            >
+              <Badge color="error">
+                <HomeIcon />
+              </Badge>
+            </StyledIconButton>
+            <StyledIconButton
+              size="large"
+              aria-label="Create post"
+              color="inherit"
+            >
+              <Badge color="error">
+                <CreateIcon />
+              </Badge>
+            </StyledIconButton>
+            <Avatar
+              onClick={handleProfileMenuOpen}
+              alt="Remy Sharp"
+              href="https://pin.it/5LeSeoE"
+            />
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <StyledIconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </StyledIconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
 }
-
-export default Nav
--- team-5-pinterest-clone/client/src/Components/OnePost.jsx
+++ team-5-pinterest-clone/client/src/Components/OnePost.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+//import IconButton from "@material-ui/core/IconButton";
+import { MDBCardText } from "mdb-react-ui-kit";
 function OnePost(props) {
   return (
     <div
@@ -24,7 +27,7 @@ function OnePost(props) {
           role="alert"
           aria-live="assertive"
           aria-atomic="true"
-          style={{ maxWidth: "400px", maxHeight: "600px" }}
+          style={{ width: "500px" }}
         >
           <div className="toast-header ">
             <div className="media d-flex">
@@ -37,69 +40,82 @@ function OnePost(props) {
                     width="30px"
                     height="30px"
                   />{" "}
-                  <h4 className="media-heading user_name">{props.user.username}</h4>
+                  <h5 className="media-heading user_name px-2">
+                    {props.user.username}
+                  </h5>
                 </span>
               </a>
             </div>
             <strong className="me-auto"></strong>
             <button
-              type="button"
-              className="btn btn-danger"
+              type="button "
+              className="btn btn-danger p-3"
               data-bs-dismiss=""
               aria-label=""
             >
-              <i class="bi bi-save">save</i>
+              <FavoriteIcon />
             </button>{" "}
+            <button type="button" className="btn p-3">
+              <ShareIcon />
+            </button>
             <button
-              type="button"
+              type="button "
               className="btn-close"
               data-bs-dismiss="toast"
               aria-label="Close"
-              onClick={()=>props.set(false)}
+              onClick={() => props.set(false)}
             ></button>
           </div>
           <div className="card-body">
             <h5>Title</h5>
             <small>{props.one.createdAt}</small>
             <h5>Description</h5>
-            <p>
-              {props.one.description}
-            </p>{" "}
-            <h5>Comments</h5>
+            <div className="p-4" style={{ backgroundColor: "#f8f9fa" }}>
+              <MDBCardText className="font-italic ">
+                {props.one.description}
+              </MDBCardText>
+            </div>
           </div>
           <div className="toast-body">
+            <h5>Comments</h5>
             <div className="media">
-              <p className="pull-right">
-                <small>5 days ago</small>
-              </p>
-              <div className="media d-flex">
-                <a className="media-left" href="#">
-                  <span className="d-flex">
-                    <img
-                      src="https://th.bing.com/th/id/OIP.BVbNgsb0pic_Ju-OKXrU3QAAAA?w=270&h=270&rs=1&pid=ImgDetMain"
-                      alt="User Avatar"
-                      className="img-circle"
-                      width="30px"
-                      height="30px"
-                    />{" "}
-                    <h4 className="media-heading user_name">Baltej Singh</h4>
-                  </span>
-                </a>
-              </div>
               <div className="media-body ">
-                Wow! this is really great.
-                <p>
-                  <small>
-                    <a href="#">Like</a> - <a href="#">Share</a>
-                  </small>
-                </p>
+                <div className="p-2" style={{ backgroundColor: "#f8f9fa" }}>
+                  <MDBCardText className="font-italic mb-1">
+                    <div className="media d-flex">
+                      <a className="media-left" href="#">
+                        <span className="d-flex">
+                          <img
+                            src="https://th.bing.com/th/id/OIP.BVbNgsb0pic_Ju-OKXrU3QAAAA?w=270&h=270&rs=1&pid=ImgDetMain"
+                            alt="User Avatar"
+                            className="img-circle"
+                            width="30px"
+                            height="30px"
+                          />{" "}
+                          <h4 className="media-heading user_name px-2">
+                            Baltej Singh
+                          </h4>
+                        </span>
+                        <p className="pull-right">
+                          <small>5 days ago</small>
+                        </p>
+                      </a>
+                    </div>
+                    Wow! this is really great.
+                    <p>
+                      <small>
+                        <a href="#">Like</a> - <a href="#">Share</a>
+                      </small>
+                    </p>
+                  </MDBCardText>
+                </div>
               </div>
             </div>
           </div>
           <div className="mb-3 card-body">
             <p>8 Comments</p>
             <div className="media d-flex">
-              <figure className="figure">
+              <figure className="figure px-2">
                 <img
                   src="https://th.bing.com/th/id/OIP.BVbNgsb0pic_Ju-OKXrU3QAAAA?w=270&h=270&rs=1&pid=ImgDetMain"
                   className="figure-img img-fluid rounded"
@@ -112,18 +128,20 @@ function OnePost(props) {
                   <div class="form-floating">
                     <textarea
                       className="form-control"
-                      placeholder="Leave a comment here"
-                      id="floatingTextarea2"
-                      control-id="ControlID-17"
-                    ></textarea>
+                      id="formGroupExampleInput"
+                      placeholder="Add a detailed description"
+                      name="Description"
+                      type="text"
+                      required
+                    />
                   </div>
-                  <span className="input-group-text">
-                    <label htmlFor="formFileLg" className="bi bi-image">
+                  <span className="input-group-text ">
+                    <label htmlFor="formFileLg" className="bi bi-image ">
                       <i className="visually-hidden">Choose an Image</i>
                       <input
                         //onChange={handleImageChange}
                         style={{ display: "none" }}
-                        className="form-control form-control-lg"
+                        className="form-control form-control-lg px-2 "
                         id="formFileLg"
                         type="file"
                       />
@@ -131,7 +149,9 @@ function OnePost(props) {
                   </span>
 
                   <span>
-                    <button type="button">done</button>
+                    <button className="btn btn-danger px-2 " type="button">
+                      ✅
+                    </button>
                   </span>
                 </div>
               </label>
