192a3ca4f905e1820331eb2f2f4924d30f886a7f
salmenkhelifi1 <salmen@base.tn>
2023-12-17 00:19:35 +0100

    comment scroll

--- team-5-pinterest-clone/client/src/Components/OnePost.jsx
+++ team-5-pinterest-clone/client/src/Components/OnePost.jsx
@@ -5,63 +5,43 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-//import IconButton from "@material-ui/core/IconButton";
 import { MDBCardText } from "mdb-react-ui-kit";
 import { RWebShare } from "react-web-share";
+
 function OnePost(props) {
   const [comment, setComment] = useState([]);
   const [refresh, setRefresh] = useState(false);
+  const [commentText, setCommentText] = useState("");
   const navigate = useNavigate();
-  // useEffect(() => {
-  //   axios
-  //     .get(`http://localhost:8800/api/users/getOneComment/${props.one.idpostes}`)
-  //     .then((result) => {
-  //       result.data.map((comments,i)=>{
-  //         axios.get(`http://localhost:8800/api/users/getOneUserid/${comments.users_idUsers}`)
-  //         .then((res)=>{
-  //           setComment([{...comments,userInfo:res.data[0]}])
-  //         }).catch((err)=>console.error(err))
-  //       })
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //     });
-  // }, []);
+
   useEffect(() => {
-    // Fetch comments for a specific post ID from the backend API
     axios
       .get(
         `http://localhost:8800/api/users/getOneComment/${props.one.idpostes}`
       )
       .then((result) => {
-        // Create an array of promises to fetch user info for each comment
         const commentPromises = result.data.map((comment) => {
-          // For each comment, fetch user info based on the user ID associated with the comment
           return axios.get(
             `http://localhost:8800/api/users/getOneUserid/${comment.users_idUsers}`
           );
         });
 
-        // Resolve all promises to get user info for each comment
         Promise.all(commentPromises)
           .then((responses) => {
-            // Combine each comment with its corresponding user info
             const updatedComments = result.data.map((comment, i) => {
               return {
                 ...comment,
-                userInfo: responses[i].data[0], // Attaching user info to the comment
+                userInfo: responses[i].data[0],
               };
             });
-
-            // Update state with comments including user info
             setComment(updatedComments);
           })
-          .catch((err) => console.error(err)); // Handle any errors in resolving promises
+          .catch((err) => console.error(err));
       })
       .catch((err) => {
-        console.error(err); // Handle errors in fetching comments from the backend API
+        console.error(err);
       });
-  }, [props.one.idpostes, refresh]); // Trigger this effect whenever props.one.idpostes changes
+  }, [props.one.idpostes, refresh]);
 
   const handleLike = (id, likes) => {
     axios
@@ -71,10 +51,33 @@ function OnePost(props) {
       .then((res) => setRefresh(!refresh))
       .catch((err) => console.log(err));
   };
+
   const handleClick = () => {
     console.log("clicked");
     navigate("/profile");
   };
+  // handleCommentAdd not work
+  // const handleCommentAdd = () => {
+  //   if (commentText.trim() === "") {
+  //     alert("Please enter a comment before submitting.");
+  //     return;
+  //   }
+
+  //   const commentData = {
+  //     postes_idpostes: props.one.idpostes,
+  //     body: commentText,
+  //   };
+
+  //   axios
+  //     .post("http://localhost:8800/api/users/addComment", commentData, {
+  //       withCredentials: true,
+  //     })
+  //     .then((res) => {
+  //       setRefresh(!refresh);
+  //       setCommentText("");
+  //     })
+  //     .catch((err) => console.error(err));
+  // };
   return (
     <div
       className="d-flex flex-column justify-content-center align-items-center vh-100 card "
@@ -110,7 +113,7 @@ function OnePost(props) {
                     className="img-circle"
                     width="30px"
                     height="30px"
-                  />{" "}
+                  />
                   <h5
                     className="media-heading user_name px-2"
                     onClick={handleClick}
@@ -131,9 +134,8 @@ function OnePost(props) {
             </button>{" "}
             <RWebShare
               data={{
-                text: "Check out this post by " + props.user.username,
-                url: "your-post-url", // Replace with the actual URL of your post
-                title: "Post by " + props.user.username,
+                text: `Check out this post by  + ${props.one.title}`,
+                title: `Post by  + ${props.user.username}`,
               }}
               onClick={() => console.log("shared successfully!")}
             >
@@ -150,68 +152,93 @@ function OnePost(props) {
             ></button>
           </div>
           <div className="card-body">
-            <h5>Title</h5>
-            <small>{props.one.createdAt}</small>
-            <h5>Description</h5>
-            <div className="p-4" style={{ backgroundColor: "#f8f9fa" }}>
-              <MDBCardText className="font-italic ">
-                {props.one.description}
-              </MDBCardText>
+            <div className="p-0" style={{ backgroundColor: "#f8f9fa" }}>
+              <h6>{props.one.title}</h6>
+              <small>{props.one.createdAt}</small>
+              <p>{props.one.description}</p>
             </div>
           </div>
-          <div className="toast-body">
-            <h5>Comments</h5>
-            <div className="media">
-              {comment.map((comment, i) => {
-                return (
-                  <div className="media-body ">
-                    <div className="p-2" style={{ backgroundColor: "#f8f9fa" }}>
-                      <MDBCardText className="font-italic mb-1">
-                        <div className="media d-flex">
-                          <a className="media-left" href="#">
-                            <span className="d-flex">
-                              <img
-                                src={comment.userInfo.photo}
-                                alt="User Avatar"
-                                className="img-circle"
-                                width="30px"
-                                height="30px"
-                              />{" "}
-                              <h4 className="media-heading user_name px-2">
-                                {comment.userInfo.username}
-                              </h4>
-                            </span>
-                            <p className="pull-right">
-                              <small>{comment.createdAt}</small>
-                            </p>
-                          </a>
-                        </div>
-                        {comment.body}
-                        <p>
-                          <small>
-                            {comment.comment_like}{" "}
-                            <a
-                              href="#"
-                              onClick={() =>
-                                handleLike(
-                                  comment.idcomment,
-                                  comment.comment_like
-                                )
-                              }
+          <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseOne"
+                  aria-expanded="false"
+                  aria-controls="collapseOne"
+                >
+                  <p>{comment.length} Comments</p>
+                </button>
+              </h2>
+              <div
+                id="collapseOne"
+                class="accordion-collapse collapse"
+                data-bs-parent="#accordionExample"
+              >
+                <div class="accordion-body">
+                  <div className="toast-body">
+                    <div
+                      className="media"
+                      style={{ maxHeight: "150px", overflowY: "auto" }}
+                    >
+                      {comment.map((comment, i) => {
+                        return (
+                          <div className="media-body" key={i}>
+                            <div
+                              className="p-2"
+                              style={{ backgroundColor: "#f8f9fa" }}
                             >
-                              Like
-                            </a>
-                          </small>
-                        </p>
-                      </MDBCardText>
+                              <MDBCardText className="font-italic mb-1">
+                                <div className="media d-flex">
+                                  <a className="media-left" href="#">
+                                    <span className="d-flex">
+                                      <img
+                                        src={comment.userInfo.photo}
+                                        alt="User Avatar"
+                                        className="img-circle"
+                                        width="30px"
+                                        height="30px"
+                                      />
+                                      <h4 className="media-heading user_name px-2">
+                                        {comment.userInfo.username}
+                                      </h4>
+                                    </span>
+                                    <p className="pull-right">
+                                      <small>{comment.createdAt}</small>
+                                    </p>
+                                  </a>
+                                </div>
+                                {comment.body}
+                                <p>
+                                  <small>
+                                    {comment.comment_like}{" "}
+                                    <a
+                                      href="#!"
+                                      onClick={() =>
+                                        handleLike(
+                                          comment.idcomment,
+                                          comment.comment_like
+                                        )
+                                      }
+                                    >
+                                      Like
+                                    </a>
+                                  </small>
+                                </p>
+                              </MDBCardText>
+                            </div>
+                          </div>
+                        );
+                      })}
                     </div>
                   </div>
-                );
-              })}
+                </div>
+              </div>
             </div>
           </div>
           <div className="mb-3 card-body">
-            <p>{comment.length} Comments</p>
             <div className="media d-flex">
               <figure className="figure px-2">
                 <img
@@ -237,6 +264,8 @@ function OnePost(props) {
                         label="comment"
                         id="fullWidth"
                         placeholder="Add comment"
+                        value={commentText}
+                        onChange={(e) => setCommentText(e.target.value)}
                       />
 
                       <span
@@ -265,6 +294,7 @@ function OnePost(props) {
                             style={{
                               paddingBottom: "10px",
                             }}
+                            //onClick={handleCommentAdd}
                           >
                             <br />
                             <i className="bi bi-send"></i> <br />
