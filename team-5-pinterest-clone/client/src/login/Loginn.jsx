7352d3a34704e385e782c057f5eb952099e50ddf
salmenkhelifi1 <salmen@base.tn>
2023-12-16 18:33:35 +0100

    login

--- team-5-pinterest-clone/client/src/login/Loginn.jsx
+++ team-5-pinterest-clone/client/src/login/Loginn.jsx
@@ -1,8 +1,132 @@
-import { useContext, useState } from "react";
+// import { useContext, useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { AuthContext } from "../authContext";
+// import { toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import "../login/login.css";
+
+// const Loginn = () => {
+//   const [inputs, setInputs] = useState({
+//     username: "",
+//     password: "",
+//   });
+//   const [err, setErr] = useState(null);
+
+//   const navigate = useNavigate();
+
+//   const handleChange = (e) => {
+//     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+//   };
+//   const { login } = useContext(AuthContext);
+//   const data = localStorage.getItem("user");
+//   console.log(data);
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+//     try {
+//       await login(inputs);
+
+//       toast.success("Login successful!", {
+//         position: "bottom-center",
+//         autoClose: 3000,
+//         hideProgressBar: true,
+//         closeOnClick: true,
+//         pauseOnHover: true,
+//         draggable: true,
+//       });
+//       navigate("/all-posts");
+//     } catch (err) {
+//       setErr(err.response.data);
+
+//       toast.error("Invalid username or password!", {
+//         position: "bottom-center",
+//         autoClose: 3000,
+//         hideProgressBar: true,
+//         closeOnClick: true,
+//         pauseOnHover: true,
+//         draggable: true,
+//       });
+//     }
+//   };
+//   return (
+//     <div className="d-flex justify-content-center align-items-center vh-100">
+//       <form>
+//         {/* Email input */}
+//         <div className="form-outline mb-4">
+//           <input
+//             type="text"
+//             placeholder="Username"
+//             name="username"
+//             id="form2Example1"
+//             className="form-control"
+//             onChange={handleChange}
+//           />
+//           <label className="form-label" htmlFor="form2Example1">
+//             Email address
+//           </label>
+//         </div>
+
+//         {/* Password input */}
+//         <div className="form-outline mb-4">
+//           <input
+//             type="password"
+//             id="form2Example2"
+//             className="form-control"
+//             name="password"
+//             onChange={handleChange}
+//           />
+//           <label className="form-label" htmlFor="form2Example2">
+//             Password
+//           </label>
+//         </div>
+
+//         <div className="row mb-4">
+//           <div className="col d-flex justify-content-center">
+//             <div className="form-check">
+//               <input
+//                 className="form-check-input"
+//                 type="checkbox"
+//                 value=""
+//                 id="form2Example31"
+//                 defaultChecked
+//               />
+//               <label className="form-check-label" htmlFor="form2Example31">
+//                 Remember me
+//               </label>
+//             </div>
+//           </div>
+
+//           <div className="col">
+//             <a href="#!">Forgot password?</a>
+//           </div>
+//         </div>
+//         {err && err}
+//         {/* Submit button */}
+//         <button
+//           type="button"
+//           className="btn btn-danger btn-block mb-4"
+//           onClick={handleLogin}
+//         >
+//           Sign in
+//         </button>
+
+//         <div className="text-center">
+//           <p>Not a member? </p>
+//           <button className="btn btn-danger ">Register</button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Loginn;
+
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../authContext";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "../login/login.css";
 import "../login/login.css";
 
 const Loginn = () => {
@@ -13,17 +137,18 @@ const Loginn = () => {
   const [err, setErr] = useState(null);
 
   const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
 
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    // Clear previous error on input change
+    setErr(null);
   };
-  const { login } = useContext(AuthContext);
-  const data = localStorage.getItem("user");
-  console.log(data);
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
+      // Assuming login is a function that handles authentication
       await login(inputs);
 
       toast.success("Login successful!", {
@@ -34,6 +159,8 @@ const Loginn = () => {
         pauseOnHover: true,
         draggable: true,
       });
+
+      // Redirect to the "/all-posts" route upon successful login
       navigate("/all-posts");
     } catch (err) {
       setErr(err.response.data);
@@ -50,40 +177,69 @@ const Loginn = () => {
   };
 
   return (
-    <div className="login">
-      <div className="card">
-        <div className="left">
-          <h1>Hello World.</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-            alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.
-          </p>
-          <span>Don't you have an account?</span>
-          <Link to="/register">
-            <button>Register</button>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <form onSubmit={handleLogin}>
+        <div className="form-outline mb-4">
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            id="form2Example1"
+            className="form-control"
+            onChange={handleChange}
+          />
+          <label className="form-label" htmlFor="form2Example1">
+            Username
+          </label>
+        </div>
+
+        <div className="form-outline mb-4">
+          <input
+            type="password"
+            id="form2Example2"
+            className="form-control"
+            name="password"
+            onChange={handleChange}
+          />
+          <label className="form-label" htmlFor="form2Example2">
+            Password
+          </label>
+        </div>
+
+        <div className="row mb-4">
+          <div className="col d-flex justify-content-center">
+            <div className="form-check">
+              <input
+                className="form-check-input "
+                type="checkbox"
+                value=""
+                id="form2Example31"
+                defaultChecked
+              />
+              <label className="form-check-label " htmlFor="form2Example31">
+                Remember me
+              </label>
+            </div>
+          </div>
+
+          <div className="col">
+            <Link to="#!">Forgot password?</Link>
+          </div>
+        </div>
+
+        {err && <div className="text-danger">{err}</div>}
+
+        <button type="submit" className="btn btn-danger btn-block mb-4">
+          Sign in
+        </button>
+
+        <div className="text-center">
+          <p>Not a member? </p>
+          <Link to="/register" className="btn btn-danger">
+            Register
           </Link>
         </div>
-        <div className="right">
-          <h1>Login</h1>
-          <form>
-            <input
-              type="text"
-              placeholder="Username"
-              name="username"
-              onChange={handleChange}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-            />
-            {err && err}
-            <button onClick={handleLogin}>Login</button>
-          </form>
-        </div>
-      </div>
+      </form>
     </div>
   );
 };
