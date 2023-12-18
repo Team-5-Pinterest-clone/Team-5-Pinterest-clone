b44eefc28014eda5cfb6e8513e84bf21e95637dd
salimhanchaoui <s.hanchaoui@yahoo.fr>
2023-12-18 04:23:15 +0100

    all done

---
+++ team-5-pinterest-clone/backend/schemausers.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM pinterestclone.users
+LIMIT 0, 1000
+
+-- Date: 2023-12-18 04:18
+*/
+INSERT INTO `` (`idUsers`,`username`,`email`,`photo`,`bio`,`password`) VALUES (1,'ElenaEvergreen27','elena.evergreen@example.com','https://i.pinimg.com/564x/02/5c/15/025c15f28201eb2d2a69df0a8a54efeb.jpg','Passionate botanist and environmental activist dedicated to preserving nature\'s wonders.','Elena@123');
+INSERT INTO `` (`idUsers`,`username`,`email`,`photo`,`bio`,`password`) VALUES (2,'NatureExplorer88','explorer@example.com','https://i.pinimg.com/564x/a1/67/71/a167719be6569e6860bea2e8cbc03faf.jpg','Adventurous soul passionate about exploring and documenting diverse ecosystems.','Explorer@2023');
+INSERT INTO `` (`idUsers`,`username`,`email`,`photo`,`bio`,`password`) VALUES (3,'GreenThumbLily','lily.gardener@example.com','https://i.pinimg.com/564x/e1/46/65/e14665f21fd2505fe9b60846a5684c12.jpg','Dedicated gardener fascinated by the magic of plants and their healing properties.','GardenLily!22');
+INSERT INTO `` (`idUsers`,`username`,`email`,`photo`,`bio`,`password`) VALUES (4,'WildlifeGuardian','guardian@example.com','https://i.pinimg.com/564x/19/e3/eb/19e3eb297782a6d41cdba70b602a065b.jpg','Protector of wildlife habitats, committed to ensuring the safety of endangered species.','Guardian123!');
+INSERT INTO `` (`idUsers`,`username`,`email`,`photo`,`bio`,`password`) VALUES (5,'webdev123','webdev@example.com','webdev_photo.jpg','Passionate web developer with expertise in front-end technologies.','securepassword123');
+INSERT INTO `` (`idUsers`,`username`,`email`,`photo`,`bio`,`password`) VALUES (6,'art','art@art.com','https://th.bing.com/th/id/R.e4d928e389236bdeaba17a65ec896257?rik=jlgaVrbnoKo2SQ&riu=http%3a%2f%2fpbs.twimg.com%2fmedia%2fDcxjVaOXkAAH9Zh.jpg&ehk=mMXbtIYROkshGz%2bcLDuF4lw6x4tpopRiILzvZs1k4ns%3d&risl=&pid=ImgRaw&r=0','Talented artist specializing in painting and sculpture, bringing creativity to life.','art123');
+INSERT INTO `` (`idUsers`,`username`,`email`,`photo`,`bio`,`password`) VALUES (7,'montasar','monta@example.com','https://th.bing.com/th/id/R.e4d928e389236bdeaba17a65ec896257?rik=jlgaVrbnoKo2SQ&riu=http%3a%2f%2fpbs.twimg.com%2fmedia%2fDcxjVaOXkAAH9Zh.jpg&ehk=mMXbtIYROkshGz%2bcLDuF4lw6x4tpopRiILzvZs1k4ns%3d&risl=&pid=ImgRaw&r=0','Talented artist specializing in painting and sculpture, bringing creativity to life.','$2a$10$4b7Y3iY9eIJIxd7uZVzjveD9JkpTpQm4SxdSEo2m6PBEoVLin/QkW');
